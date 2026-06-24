83199464929b142d0e8abf99fe39406d017c2bd6
IURII VESELSKII <you@example.com>
2026-06-25 01:03:08 +0500

    Add multiply function

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log('Hello World');
 function add(a, b) { return a + b; }
 function add(a, b) { return a + b; }
 function multiply(a, b) { return a * b; }
+function multiply(a, b) { return a * b; }
